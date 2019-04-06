16689610938015e88cb15ad6e5e2f3e5bb2872bd
Dimitri Walters <me@dimi.io>
2019-04-05 22:55:49 -0400

    Add another template file
    This file is for input of single int and string

---
+++ int-string.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+def sol(t, n, p):
+    print('Case #%d: %s' % (t, 'a'))
+    return
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for i in range(t):
+        n = int(input())
+        p = str(input())
+
+        sol(i + 1, n, p)
